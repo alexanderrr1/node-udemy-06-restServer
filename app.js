11e3778a68979e2095b69150fde6ea5fc00f4f5b
Alexander Reineck <alexreineck@gmail.com>
2021-12-14 10:51:17 -0300

    restServer

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const Server = require('./models/servers');
+
+const server = new Server();
+
+server.listen();
+ 
+
+ 
---
+++ controllers/usuarios.js
@@ -0,0 +1,55 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = "No name", page = 1, limit } = req.query;
+
+    res.json({
+        msg: "get API - controlador",
+        q,
+        nombre,
+        page,
+        limit
+    });
+}
+
+const usuariosPut = (req, res) => {
+
+    const { id } = req.params;
+    
+    res.status(500).json({
+        msg: "put API - controlador",
+        id,
+    });
+};
+
+const usuariosPost = (req, res) => {
+
+    const { nombre, edad } = req.body;
+
+    res.status(201).json({
+        msg: "post API - controlador",
+        nombre,
+        edad
+    });
+};
+
+const usuariosPatch = (req, res) => {
+    res.json({
+        msg: "patch API - controlador"
+    });
+};
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: "delete API - controlador"
+    });
+};
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch,
+}
