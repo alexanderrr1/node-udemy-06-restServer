f97c5163524f082d11e5378c14f0a917d17f8b5f
Alexander Reineck <alexreineck@gmail.com>
2022-01-08 14:47:02 -0300

    Sección9 - Parte 1

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,7 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -13,28 +16,44 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPut = (req, res) => {
+const usuariosPut = async(req, res = response) => {
 
     const { id } = req.params;
-    
-    res.status(500).json({
+    const { password, google, correo, ...resto } = req.body;
+
+    // TODO validar contra base de datos
+    if( password ){
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
+
+    res.json({
         msg: "put API - controlador",
-        id,
+        usuario
     });
 };
 
-const usuariosPost = (req, res) => {
+const usuariosPost = async(req, res) => {
 
-    const { nombre, edad } = req.body;
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    // Guardar en BD
+    await usuario.save();
 
     res.status(201).json({
-        msg: "post API - controlador",
-        nombre,
-        edad
+        usuario
     });
 };
 
-const usuariosPatch = (req, res) => {
+const usuariosPatch = (req, res = response) => {
     res.json({
         msg: "patch API - controlador"
     });
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido, emailExiste } = require('../helpers/db-validators');
 
 const { 
     usuariosGet, 
@@ -14,7 +18,15 @@ router.get( '/', usuariosGet );
 
 router.put( '/:id', usuariosPut );
 
-router.post( '/', usuariosPost );
+router.post( '/', 
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe tener mas de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom( emailExiste ),
+    check('rol').custom( esRoleValido ),
+    validarCampos,
+    usuariosPost 
+);
 
 router.patch( '/', usuariosPatch );
 
