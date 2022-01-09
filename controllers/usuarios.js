b44009b55faf737d4cee60f5aafc0a3d48a9e771
Alexander Reineck <alexreineck@gmail.com>
2022-01-09 13:02:30 -0300

    Seccion10 - Autenticación de usuario - JWT

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -66,13 +66,10 @@ const usuariosDelete = async(req, res = response) => {
     
     const { id } = req.params;
 
-    // Fisicamente lo borramos
-    // const usuario = await Usuario.findByIdAndDelete( id );
-
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
 
     res.json({
-        usuario
+        usuario,
     });
 };
 
--- models/servers.js
+++ models/servers.js
@@ -8,7 +8,9 @@ class Server{
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usuariosRoutePath = "/api/usuarios";
+
+        this.usuariosRoutePath = '/api/usuarios';
+        this.authPath = '/api/auth';
 
         // Conectar a base de datos
         this.conectarDB();
@@ -37,6 +39,7 @@ class Server{
 
     routes() {
 
+        this.app.use( this.authPath, require('../routes/auth') );
         this.app.use( this.usuariosRoutePath, require('../routes/usuarios') );
 
     }
